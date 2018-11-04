65215a0c7270d665cb565c0928ae1288efaa04e0
Ivo Vošahlík <ivosahlik@seznam.cz>
2018-11-04 01:02:30 +0100

    feature-5/add-animations - added animation

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@
 
     <div class="row">
       <div>
-        <component v-bind:is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+        <transition name="flip" mode="out-in">
+          <component v-bind:is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
+        </transition>
       </div>
     </div>
   </div>
@@ -42,5 +44,37 @@ export default {
 </script>
 
 <style lang="scss">
+  .flip-enter {
+    /*transform: rotateY(0deg);*/
+  }
 
+  .flip-enter-active {
+    animation: flip-in  0.5s ease-out forwards;
+  }
+
+  .flip-leave {
+    /*transform: rotateY(0deg);*/
+  }
+
+  .flip-leave-active {
+    animation: flip-out 0.5s ease-out forwards;
+  }
+
+  @keyframes flip-out {
+    from {
+      transform: rotateY(0deg);
+    }
+    to {
+      transform: rotateY(90deg);
+    }
+  }
+
+  @keyframes flip-in {
+    from {
+      transform: rotateY(90deg);
+    }
+    to {
+      transform: rotateY(0deg);
+    }
+  }
 </style>
